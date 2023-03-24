08a948bcf7a2bfb6a90cb41f22f473436873e347
StrickVax <strickvax@gmail.com>
2023-03-24 16:08:03 -0700

    Fleshed out README

---
+++ cat_solver.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void Solver();
+
+int main()
+{
+    
+    return 0;
+}
+
+void Solver()
+{
+    // the flag is to become false if the number fails any of these conditions:
+    // --A number appears more than once
+    // --A number is larger than 60
+    // --A square root returns a non-integer number
+    bool flag = true;
+    while (flag == true)
+    {
+        
+    }
+}
