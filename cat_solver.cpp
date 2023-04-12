8abaa996acd89429839858fe8d4611e9fa0a20ff
StrickVax <strickvax@gmail.com>
2023-04-12 01:56:13 -0700

    Retreader undoes currentValue

--- cat_solver.cpp
+++ cat_solver.cpp
@@ -14,10 +14,10 @@ void solutionPrinter(vector<int> &);
 bool validResult(double, vector<int> &);
 
 // Checks to see if the number has already been used before
-bool alreadyUsedNumber(double, vector<int> &);
+bool alreadyUsedNumber(double, vector<int>);
 
 // responsible for undoing results and operations
-void retreader(vector<int> &, vector<int> &);
+void retreader(double &, vector<int> &, vector<int> &);
 
 // function that actually records results and operations
 void pusher(double &, vector<int> &, int, vector<int> &);
@@ -42,6 +42,8 @@ void Solver(vector<int> numbersNeeded, vector<int> &solutionPath)
     // the operation is recorded by a 0/1/2, which this variable does
     int solutionRecorder = 0;
 
+    int cycles = 0;
+
     // The flag becomes false when the program has gone through every possible intro
     // without a valid path
     bool flag = true;
@@ -52,23 +54,26 @@ void Solver(vector<int> numbersNeeded, vector<int> &solutionPath)
         // this block checks to see if the result is valid
         if (validResult(currentNumber, numbersAlreadyUsed))
         {
-            continue;
+            cout << " UH ";
         }
         // if it is not valid, it will undo the last operation, delete the previous result
         // and try the next operation available
         else if (solutionRecorder < 2)
         {
-            retreader(numbersAlreadyUsed, solutionPath);
+            retreader(currentNumber, numbersAlreadyUsed, solutionPath);
             solutionRecorder += 1;
-        } 
+        }
         // if sqrt is not valid, it will retread back two spaces, reset the operation to +5
         // and resume checking again
         else if (solutionRecorder >= 2)
         {
-
+            retreader(currentNumber, numbersAlreadyUsed, solutionPath);
+            retreader(currentNumber, numbersAlreadyUsed, solutionPath);
+            solutionRecorder = 0;
+            cycles = 1;
         }
 
-        else
+        else if ((solutionPath.size() == 0) && (cycles != 0))
         {
             // The flag becomes false when the program has gone through every possible intro
             // without a valid path
@@ -133,12 +138,21 @@ void solutionPrinter(vector<int> &orderOfOperations)
     }
 }
 
-bool alreadyUsedNumber(double result, vector<int> &numbersAlreadyUsed)
+bool alreadyUsedNumber(double result, vector<int> numbersAlreadyUsed)
 {
+
     // TODO: need to make sure number hasn't been repeated
     for (int i = 0; i < numbersAlreadyUsed.size(); i++)
     {
         // Maybe sort numbers at one point? Especially if the number of tries gets [ big ]
+        if (result == numbersAlreadyUsed[i])
+        {
+            // there's an issue where it will mark the number it [ just ] got as already-used
+            // So to compromise, I have it delete that instance, and have it return with a
+            // repeat if it finds it [twice]
+            numbersAlreadyUsed.erase(numbersAlreadyUsed.begin() + i);
+        }
+
         if (result == numbersAlreadyUsed[i])
         {
             return true;
@@ -170,8 +184,22 @@ void pusher(double &result, vector<int> &numbersAlreadyUsed, int operation, vect
     numbersAlreadyUsed.push_back(result);
 }
 
-void retreader(vector<int> &numbersAlreadyUsed, vector<int> &solutionPath)
+void retreader(double &result, vector<int> &numbersAlreadyUsed, vector<int> &solutionPath)
 {
+    switch (solutionPath.back())
+    {
+    case 0:
+        result -= 5;
+        break;
+
+    case 1:
+        result -= 7;
+        break;
+
+    case 2:
+        result = (result * result);
+        break;
+    }
     numbersAlreadyUsed.pop_back();
     solutionPath.pop_back();
 }
